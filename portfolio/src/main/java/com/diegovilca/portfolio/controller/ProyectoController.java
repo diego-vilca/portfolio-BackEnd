3e3a5b2c4fe99f80b61c8acd3e9d124dea62fb70
Diego Vilca <vicv87.mp@gmail.com>
2023-03-21 20:25:09 -0300

    modifico anotacion CrossOrigin en controladoras

--- portfolio/src/main/java/com/diegovilca/portfolio/controller/ProyectoController.java
+++ portfolio/src/main/java/com/diegovilca/portfolio/controller/ProyectoController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("project")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin//(origins = {"http://localhost:4200"})
 public class ProyectoController {
     
     @Autowired
